a61cc7c5ccf6eb19390415c17529b86f465beeb5
Ben Willmore <ben@willmore.eu>
2025-01-13 20:17:23 +0000

    Add tests

--- test-dl.sh
+++ test-dl.sh
@@ -2,8 +2,47 @@
 
 # could git clone here to make sure checkout has right version of repo
 
-md5 () {
+md5file () {
     md5sum "${1}" | awk '{print $1}'
+}
+
+md5dir () {
+  find "${1}" -type f -exec md5sum {} + | awk '{print $1}' | sort | md5sum | awk '{print $1}'
+}
+
+setup () {
+  echo == Test: $1
+  rm -rf "${test_dir}"
+  mkdir "${test_dir}"
+  cd "${test_dir}"
+}
+
+test_file () {
+  if [[ $(md5file "${1}") != $(md5file "${2}") ]]; then
+    echo Fail
+    exit 1
+  fi
+}
+
+test_dir () {
+  if [[ $(md5dir "${1}") != $(md5dir "${2}") ]]; then
+    echo Fail
+    exit 1
+  fi
+}
+
+test_fail () {
+  if [[ $? == 0 ]]; then
+    echo Should have failed and did not
+    exit 1
+  else
+    echo OK -- failure expected
+  fi
+}
+
+teardown () {
+  cd "${root_dir}"
+  rm -rf "${test_dir}"
 }
 
 github_fetch="$(pwd)/github-fetch"
@@ -15,49 +54,37 @@ check_dir="$(pwd)/test"
 
 rm -rf "${test_dir}"
 
-echo "Downloading single file to current directory"
-mkdir "${test_dir}"
-cd "${test_dir}"
+setup "Downloading single file to current directory"
 $github_fetch https://github.com/ben-willmore/github-fetch/blob/main/test/subdir/three
-if [[ $(md5 $test_dir/three) != $(md5 $check_dir/subdir/three) ]]; then
-  echo Fail
-  exit 1
-fi
-cd "${root_dir}"
-rm -rf "${test_dir}"
+test_file $test_dir/three $check_dir/subdir/three
+teardown
 
-echo "Downloading single file to current directory - should fail"
-rm -rf "${test_dir}"
-mkdir "${test_dir}"
-cd "${test_dir}"
+setup "Downloading single file to current directory - blocked, should fail"
 touch three
 $github_fetch https://github.com/ben-willmore/github-fetch/blob/main/test/subdir/three
-if [[ $? == 0 ]]; then
-  echo Should have failed and did not
-  exit 1
-fi
-cd "${root_dir}"
-rm -rf "${test_dir}"
+test_fail
+teardown
 
-echo "Downloading single file to alternate directory"
-rm -rf "${test_dir}"
-mkdir "${test_dir}"
-$github_fetch https://github.com/ben-willmore/github-fetch/blob/main/test/subdir/three "${test_dir}"
-if [[ $(md5 $test_dir/three) != $(md5 $check_dir/subdir/three) ]]; then
-  echo Fail
-  exit 1
-fi
-cd "${root_dir}"
-rm -rf "${test_dir}"
+setup "Downloading single file to alternate directory"
+mkdir ./alt_dir
+$github_fetch https://github.com/ben-willmore/github-fetch/blob/main/test/subdir/three "./alt_dir"
+test_file ./alt_dir/three $check_dir/subdir/three
+teardown
 
-echo "Downloading single file to alternate directory - should fail"
-rm -rf "${test_dir}"
-mkdir "${test_dir}"
-touch "${test_dir}/three"
-$github_fetch https://github.com/ben-willmore/github-fetch/blob/main/test/subdir/three "${test_dir}"
-if [[ $? == 0 ]]; then
-  echo Should have failed and did not
-  exit 1
-fi
-cd "${root_dir}"
-rm -rf "${test_dir}"
+setup "Downloading single file to alternate directory - blocked, should fail"
+mkdir ./alt_dir
+touch ./alt_dir/three
+$github_fetch https://github.com/ben-willmore/github-fetch/blob/main/test/subdir/three "./alt_dir"
+test_fail
+teardown
+
+setup "Downloading subdir to current directory"
+$github_fetch https://github.com/ben-willmore/github-fetch/tree/main/test
+test_dir ./test $check_dir
+teardown
+
+setup "Downloading subdir to current directory - blocked, should fail"
+touch test
+$github_fetch https://github.com/ben-willmore/github-fetch/tree/main/test
+test_fail
+teardown
