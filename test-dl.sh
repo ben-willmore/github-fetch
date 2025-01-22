c1761a6a1078e81a72a911c3cd5560168d473ecc
Ben Willmore <ben@willmore.eu>
2025-01-22 21:43:19 +0000

    Update tests

--- test-dl.sh
+++ test-dl.sh
@@ -8,8 +8,8 @@
 # * the remaining special chars -- *, ...?
 
 ignore_api_limit=false
-#cache_flag=
-cache_flag='--test --curl'
+clear_cache=false
+cache_flag='--test'
 
 urldecode () {
   local data=${1//+/ }
@@ -18,16 +18,17 @@ urldecode () {
 
 urlencode () {
   proto=$(echo "$1" | grep :// | sed -e's,^\(.*://\).*,\1,g')
-  printf "${proto}"
   url="$(echo ${1/$proto/})"
-  local l=${#url}
+  printf "${proto}"
+  local l="${#url}"
   for (( i = 0 ; i < l ; i++ )); do
-    local c=${url:i:1}
+    local c="${url:i:1}"
     case "$c" in
-      [a-zA-Z0-9.~_-]) printf "$c" ;;
-      '/') printf / ;;
-      ' ') printf + ;;
-      *) printf '%%%.2X' "'$c"
+      # / is missing, becaue that would also translate the separators. Probably we should really
+      # separate out the bits of the URL and encode each one. I'm not sure it makes a difference
+      # unless you can have / in a git filename. Maybe on windows?
+      [:?\#\[\]@!$%\&\'\(\)\*\+\,\;={}\ ]) printf '%%%.2X' "'$c" ;;
+      *) printf "$c" ;;
     esac
   done
 }
@@ -84,7 +85,12 @@ git_checkout () {
 }
 
 initial_setup () {
+  if [[ $clear_cache == true ]]; then
+    rm -rf cache/
+  fi
+
   github_fetch="$(pwd)/github-fetch --verbose $cache_flag"
+  github_fetch_curl_nocache="$(pwd)/github-fetch --verbose --curl"
 
   root_dir="$(pwd)"
   test_dir="$(pwd)/test.tmp"
@@ -121,6 +127,12 @@ $github_fetch https://github.com/ben-willmore/github-fetch/blob/main/test/subdir
 test_file $test_dir/three $check_dir/subdir/three
 teardown
 
+setup "Downloading single file to current directory -- curl"
+git_checkout main
+$github_fetch_curl_nocache https://github.com/ben-willmore/github-fetch/blob/main/test/subdir/three
+test_file $test_dir/three $check_dir/subdir/three
+teardown
+
 setup "Downloading single file to current directory - blocked, should fail"
 git_checkout main
 touch three
@@ -141,9 +153,21 @@ $github_fetch https://github.com/ben-willmore/github-fetch
 test_dir $test_dir/github-fetch/test $check_dir
 teardown
 
+setup "Downloading whole repo to current directory -- curl"
+git_checkout main
+$github_fetch_curl_nocache https://github.com/ben-willmore/github-fetch
+test_dir $test_dir/github-fetch/test $check_dir
+teardown
+
 setup "Downloading subdir to current directory"
 git_checkout main
 $github_fetch https://github.com/ben-willmore/github-fetch/tree/main/test
+test_dir $test_dir/test $check_dir
+teardown
+
+setup "Downloading subdir to current directory -- curl"
+git_checkout main
+$github_fetch_curl_nocache https://github.com/ben-willmore/github-fetch/tree/main/test
 test_dir $test_dir/test $check_dir
 teardown
 
@@ -178,8 +202,6 @@ $github_fetch https://github.com/ben-willmore/github-fetch/blob/b705c8521553926a
 test_file "$test_dir/s e v e n" "$check_dir/sub dir/s e v e n"
 teardown
 
-exit 0
-
 setup "Downloading subdir to current directory -- specific commit"
 git_checkout b705c8521553926a84609816d3f5ed8814945aee
 $github_fetch https://github.com/ben-willmore/github-fetch/tree/b705c8521553926a84609816d3f5ed8814945aee/test
@@ -198,49 +220,58 @@ $github_fetch https://github.com/ben-willmore/github-fetch/tree/17defefbffd7b4a0
 test_dir $test_dir/subdir $check_dir/subdir/subdir/subdir/subdir
 teardown
 
-# special chars -- some are missing, such as *
-chars='! %22 %23 %24 %25 %26 '"'"' ( ) %2B %2C - %3B %3C %3D %3E %40 %5D _ %60 %7B %7B %7C %7D %7F'
-
+# download files with special characters individually
 base_url="https://github.com/ben-willmore/github-fetch/blob/main/test/special-chars/"
-for char in $chars; do
-  initial_setup
-  url="${base_url}${char}"
-  base=$(basename ${url})
-  base=$(urldecode ${base})
-  setup "Downloading file with special char \"${base}\" in filename"
-  git_checkout main
-  $github_fetch "${url}"
-  test_file "$test_dir/${base}" "$check_dir/special-chars/${base}"
-  teardown
-done
+git_checkout main
 
-if [[ $ignore_api_limit == true ]];; then
+find $check_dir/special-chars -maxdepth 1 -type f > test.tmpfile
+while IFS= read -r file; do
+  file=$(basename "${file}")
+  enc=$(urlencode "-${file}-")
+  enc="${enc:1}"
+  enc="${enc%?}"
+  url="${base_url}${enc}"
+  setup "Downloading file \"${file}\" with special char in filename"
+  $github_fetch "${url}"
+  test_file "$test_dir/${file}" "$check_dir/special-chars/${file}"
+  teardown
+done < test.tmpfile
+rm test.tmpfile
+
+if [[ 0 == 0 ]]; then #[[ $ignore_api_limit == true ]];; then
   base_url="https://github.com/ben-willmore/github-fetch/tree/main/test/special-chars/dirs/"
-  for char in $chars; do
-    initial_setup
-    url="${base_url}${char}"
-    base=$(basename ${url})
-    base=$(urldecode ${base})
-    setup "Downloading dir with special char \"${base}\" in dirname"
-    git_checkout main
+  find $check_dir/special-chars/dirs -mindepth 1 -maxdepth 1 -type d > test.tmpfile
+
+  while IFS= read -r dirname; do
+    dirname=$(basename "${dirname}")
+    enc=$(urlencode "-${dirname}-")
+    enc="${enc:1}"
+    enc="${enc%?}"
+    url="${base_url}${enc}"
+    echo --$url--
+    setup "Downloading dir \"${dirname}\" with special char in dirname"
     $github_fetch "${url}"
-    test_dir "$test_dir/${base}" "$check_dir/special-chars/dirs/${base}"
+    test_dir "$test_dir/${dirname}" "$check_dir/special-chars/dirs/${dirname}"
     teardown
-  done
+  done < test.tmpfile
+  rm test.tmpfile
 
   base_url="https://github.com/ben-willmore/github-fetch/blob/main/test/special-chars/dirs/"
-  for char in $chars; do
-    initial_setup
-    url="${base_url}${char}/testfile"
-    base=$(basename ${url})
-    base=$(urldecode ${base})
-    setup "Downloading file with special char \"${char}\" in dirname"
-    git_checkout main
+  find $check_dir/special-chars/dirs -mindepth 1 -maxdepth 1 -type d > test.tmpfile
+
+  while IFS= read -r dirname; do
+    dirname=$(basename "${dirname}")
+    enc=$(urlencode "-${dirname}-")
+    enc="${enc:1}"
+    enc="${enc%?}"
+    url="${base_url}${enc}/testfile"
+    echo --$url--
+    setup "Downloading file from \"${dirname}\" with special char in dirname"
     $github_fetch "${url}"
-    cc=$(urldecode "${char}")
-    test_file "$test_dir/testfile" "$check_dir/special-chars/dirs/${cc}/testfile"
+    test_file "$test_dir/testfile" "$check_dir/special-chars/dirs/${dirname}/testfile"
     teardown
-  done
+  done < test.tmpfile
+
 fi
 
 final_cleanup
